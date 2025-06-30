92ce7aa03c08164d87233826b17f561b80ffddbd
ZoRoEmiya <davidlevi14404@gmail.com>
2025-06-30 20:52:43 +0300

    Add database connection initialization in server startup

--- server.js
+++ server.js
@@ -8,3 +8,8 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+(async () => {
+  await connectToDB(); 
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+})();
