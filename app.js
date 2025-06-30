66d07e856146c8b46cf2ccdf2eb61bfb6edcf3e9
ZoRoEmiya <davidlevi14404@gmail.com>
2025-06-30 22:59:22 +0300

    Add 404 error handling middleware to app.js

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api', routes);
+app.use((_req, res) => res.status(404).json({ message: 'Not Found' }));
 app.use(errorHandler);
 
 export default app;
